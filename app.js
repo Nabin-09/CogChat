5ed97ef1580baaddb056af47bcd11009042c8aa4
Nabin <sharmanabin.0910@gmail.com>
2025-06-10 17:44:09 +0530

    Implemented Active Clients

--- app.js
+++ app.js
@@ -21,16 +21,25 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // Socket.IO event handling
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
+io.on('connection' , onConnected)
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+let socketsConnected = new Set()
+
+function onConnected(socket){
+    console.log(socket.id)
+    socketsConnected.add(socket.id);
+
+    io.emit('clients-total' ,socketsConnected.size)
+
+    socket.on('disconnect', ()=>{
+        console.log('Socket Disconnects' , socket.id);
+        socketsConnected.delete(socket.id);
+        io.emit('clients-total' , socketsConnected.size)
+    })
+}
 
 // Start the server
 server.listen(PORT, () => {
