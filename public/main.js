3891b4871421922847c76073007d4c34294b6aac
Nabin <sharmanabin.0910@gmail.com>
2025-06-14 20:56:46 +0530

    Restart

--- public/main.js
+++
@@ -1,6 +0,0 @@
-const socket = io()
-
-const clientsTotal = document.getElementById('clients-total')
-socket.on('clients-total' , (data)=>{
-    clientsTotal.innerText = `Total Clients : ${data}`
-})
