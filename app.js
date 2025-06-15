bef568fe4d3977fcfff3cf9879d54f5a8e04376e
Nabin <sharmanabin.0910@gmail.com>
2025-06-15 10:41:37 +0530

    Connected users

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const { log } = require('console');
 const express = require('express')
-const path = require('path')
+const path = require('path');
+const { CLIENT_RENEG_WINDOW } = require('tls');
 const app = express();
 
 
@@ -13,6 +15,19 @@ const io = require('socket.io')(server)
 
 
 app.use(express.static(path.join(__dirname,'public')))
-io.on('connect' , (socket)=>{
-    console.log(socket.id);
-})
+
+let socketsConnected = new Set();
+
+io.on('connect' , onConnected)
+function onConnected(socket){
+    console.log(socket.id)
+    socketsConnected.add(socket.id)
+
+    io.emit('clients-total' , socketsConnected.size)
+
+    socket.on('disconnect' , ()=>{
+        console.log(`Socket Disconnected : ${socket.id}`);
+        socketsConnected.delete(socket.id);
+        io.emit('clients-total' , socketsConnected.size)
+    })
+}
