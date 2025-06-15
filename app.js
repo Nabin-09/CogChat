7a4b8dd3873e44cf6d7696301c7891c4230c76b8
Nabin <sharmanabin.0910@gmail.com>
2025-06-15 10:34:48 +0530

    Setting up sockets

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express')
 const path = require('path')
 const app = express();
 
+
 const PORT = process.env.PORT || 4000
 
 const server = app.listen(PORT, ()=>{
     console.log(`Server running at ${PORT}`)
 })
 
+const io = require('socket.io')(server)
+
+
 app.use(express.static(path.join(__dirname,'public')))
+io.on('connect' , (socket)=>{
+    console.log(socket.id);
+})
